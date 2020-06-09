c85aaf286368a671e8b1642df71bbf3f3e1f190d
Iddar Olivares <iddar@dbug.mx>
2020-06-09 03:05:00 -0500

    include basic router

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+declare global {
+  interface Window { __off_router_state__: any; }
+}
+
 export type EventCallback = (ev: Event) => void
 export type CleanEvent = () => void
 export type Render = (innerHTML: string, action?: Action) => void
@@ -29,7 +33,7 @@ export function html (template: TemplateStringsArray, ...args: Array<any>): stri
   },"")
 }
 
-class Offjs {
+export class Offjs {
   private _events: CleanEvent[] = []
   private _container: HTMLElement
 
@@ -73,13 +77,6 @@ class Offjs {
   }
 }
 
-type props = {}
-type Component = (props: props)
-export const router: Component = (_props) => {
-
-}
-
-
 let instance: Offjs
 export default function off (el?: HTMLElement) {
   if (instance == null) {
@@ -87,4 +84,95 @@ export default function off (el?: HTMLElement) {
   }
   
   return instance.render.bind(instance)
+}
+
+export type props = {}
+export type Component = (render: Render, props?: props) => void
+
+export type Routes = Map<string, Component>
+
+type Router = (routes: Routes) => void
+
+export function createRouter (render: Render): Router {
+  if (render == null) throw new Error('Should include render function')
+
+  const ROOT = 'INDEX'
+
+  type RouterState = {
+    history: [string?],
+    courrent: Function,
+    perv: Function,
+  }
+
+  const state: RouterState = {
+    history: [],
+    courrent: (): string => {
+      return [...state.history].pop() as string
+    },
+    perv: (): string => {
+      if (state.history.length >= 2) {
+        state.history.pop()
+        return state.history[state.history.length - 1] as string
+      } else {
+        return state.history[0] as string
+      }
+    }
+  }
+
+  window.__off_router_state__ = state
+
+  let routes: Routes = new Map()
+
+  function to (route: string, props?: props): void {
+    const page = routes.get(route.toUpperCase())
+    if (typeof page !== 'function') {
+      throw new Error(`Expected Component function in '${route} 'route`)
+    }
+    
+    if (props && 'nav' in props) {
+      throw new Error('Invalid property: `nav` property is reserved for `Off-router`')
+    }
+
+    if (state.courrent() !== route.toUpperCase()) {
+      state.history.push(route.toUpperCase())
+    }
+
+    page(render, {
+      ...props,
+      nav: {
+        to,
+        back: (props?: props) => to(state.perv(), props),
+        courrent: () => state.courrent()
+      },
+    })
+  }
+  
+  return function router (initialRoutes: Routes) {
+    if (initialRoutes == null) {
+      throw new Error('Should include Routes: Map<\'routeName\', component>')
+    }
+    
+    for (let [route, component] of initialRoutes.entries()) {
+      if (routes.has(route.toUpperCase())) {
+        throw new Error('Expected all routes must have different names')
+      }
+      
+      if (typeof component !== 'function') {
+        throw new Error(`Expected Component function in ${route} route`)
+      }
+      
+      routes.set(route.toUpperCase(), component)
+    }
+
+    if (!routes.has(ROOT)) {
+      throw new Error('Should include `index` route')
+    }
+    
+    const page = routes.get(ROOT)
+    if (typeof page !== 'function') {
+      throw new Error('Expected Component function in `index` route')
+    }
+    
+    to(ROOT) // mount index view
+  }
 }
